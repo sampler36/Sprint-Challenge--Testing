eb822563be2a04230f9c3c10a800596281ee17d4
Talent Antonio <antonio.talenty@gmail.com>
2019-04-19 09:42:35 +0100

    tables working

--- data/seeds/01-games.js
+++ data/seeds/01-games.js
@@ -5,8 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('games').insert([
-        {id: 1, title: 'raw dogg',  genre: 'adventure'},
-        {id: 1, title: 'zulu on my stoope',  genre: 'adventure'}
+        { id:1, title: 'Raw dogg',  genre: 'adventure'},
+        { id:2, title: 'Crouching tiger',  genre: 'action'},
+        { id:3, title: 'Shanghai noon',  genre: 'action'},
+        { id:4, title: 'Zulu on my stoope',  genre: 'adventure'}
   
       ]);
     });
