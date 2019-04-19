17cdd031b57c6b1603f7f2b60bd718728d6cfb5a
Talent Antonio <antonio.talenty@gmail.com>
2019-04-19 09:23:38 +0100

    migration and seeding

---
+++ data/seeds/01-games.js
@@ -0,0 +1,13 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('games').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('games').insert([
+        {id: 1, title: 'raw dogg',  genre: 'adventure'},
+        {id: 1, title: 'zulu on my stoope',  genre: 'adventure'}
+  
+      ]);
+    });
+};
